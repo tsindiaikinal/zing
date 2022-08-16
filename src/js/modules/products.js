d6379119aedbd799608f39f843d20ad0c9a9fa5a
Alekseii <1914.ac@gmail.com>
2022-08-16 04:16:04 +0300

    [modules] add new folder and files Modernizer, Products

---
+++ src/js/modules/products.js
@@ -0,0 +1,96 @@
+export const category = [
+  {
+    id: 1,
+    name: "chair",
+  },
+  {
+    id: 2,
+    name: "table",
+  },
+  {
+    id: 3,
+    name: "bed",
+  },
+  {
+    id: 4,
+    name: "closet",
+  },
+  {
+    id: 5,
+    name: "sofa",
+  },
+  {
+    id: 6,
+    name: "cabinet",
+  },
+];
+
+export const products = [
+  {
+    categoryId: 1,
+    id: 1,
+    active: true,
+    image: "img/stool.png",
+    name: "Stool",
+    price: 200,
+  },
+  {
+    categoryId: 1,
+    id: 2,
+    active: true,
+    image: "img/light_char.png",
+    name: "Light char",
+    price: 80,
+  },
+  {
+    categoryId: 1,
+    id: 3,
+    active: true,
+    image: "img/chair.png",
+    name: "Chair",
+    price: 200,
+  },
+  {
+    categoryId: 1,
+    id: 4,
+    active: true,
+    image: "img/table_lamp.png",
+    name: "Table Lamp",
+    price: 50,
+  },
+  {
+    categoryId: 2,
+    id: 1,
+    active: true,
+    image: "img/tea_table.png",
+    name: "Tea table",
+    price: 150,
+  },
+  {
+    categoryId: 2,
+    id: 2,
+    active: true,
+    image: "img/bedside_table.png",
+    name: "Bedside table",
+    price: 100,
+  },
+  {
+    categoryId: 5,
+    id: 1,
+    active: true,
+    image: "img/sofa_light.png",
+    name: "Sofa - Light",
+    price: 300,
+  },
+  {
+    categoryId: 5,
+    id: 1,
+    active: true,
+    image: "img/sofa_dark.png",
+    name: "Sofa - Dark",
+    price: 300,
+  },
+  { categoryId: 3, id: 1, active: false, image: "img/", name: "", price: 0 },
+  { categoryId: 4, id: 1, active: false, image: "img/", name: "", price: 0 },
+  { categoryId: 6, id: 1, active: false, image: "img/", name: "", price: 0 },
+];
